da80749a72c09afcfb8d1907464f58f2d11aeef6
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2015-05-04 14:12:46 -0500

    entidad -crear articulo y ver todos los articulos

--- src/Entidades/Articulo.java
+++ src/Entidades/Articulo.java
@@ -5,41 +5,53 @@ import Database.Database;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Articulo {
 
     private int id = -1;
-    private String tipoPublicacion;
-    private Date fechaPublicacion;
-    private Autor autor;
-    private Juez juez;
+    private String titulo, texto;
+    private List<Integer> idEditores;
 
-    public Articulo(String tipoPub, Date f, Autor a, Juez j) {
-        tipoPublicacion = tipoPub;
-        fechaPublicacion = f;
-        autor = a;
-        juez = j;
+    public Articulo(String titulo, String texto, List<Integer> editores) {
+        this.titulo = titulo;
+        this.texto = texto;
+        this.idEditores = new ArrayList<>(editores);
     }
 
     public static List<Articulo> getAll() {
-        ResultSet rs = Database.query("SELECT id, tipo, fecha_de_publicacion, publicado, autorizado FROM Articulo");
-        List<Articulo> articulos = new ArrayList<>();
+        ResultSet rs = Database.query("SELECT id, titulo, texto, Editor.id from Articulo JOIN Editores_Articulos on id_articulo = Articulo.id JOIN Editor ON Editor.id = id_editor");
+        Map<Integer, Articulo> articulos = new HashMap<>();
         try {
             while(rs.next()) {
-                articulos.add(new Articulo(rs.getString("tipo"), rs.getTimestamp("fecha_de_publicacion"), null, null));
+                int id = rs.getInt("Articulo.id");
+                if (!articulos.containsKey(id)) {
+                    articulos.put(id, new Articulo(rs.getString("titulo"), rs.getString("texto"), new ArrayList<>()));
+                }
+                
+                int idEditor = rs.getInt("Editor.id");
+                articulos.get(id).idEditores.add(idEditor);
             }
         } catch (SQLException ex) {
             Logger.getLogger(Articulo.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        return articulos;
+        return new ArrayList<>(articulos.values());
     }
     
     public void guardar() {
-        Database.update("INSERT INTO Articulo () VALUES ()", args);
+        try {
+            Database.update("INSERT INTO Articulo (titulo, texto) VALUES ('%s', '%s')", this.titulo, this.texto);
+            ResultSet rs = Database.query("SELECT id FROM Articulo ORDER BY id DESC LIMIT 1");
+            this.id = !rs.next() ? -1 : rs.getInt(1);
+        } catch (SQLException ex) {
+            Logger.getLogger(Articulo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    
+    
 }
