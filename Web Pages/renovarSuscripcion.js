a465f5b8336d9886ee287b43ad3c1e821285c586
Luis-Lamat <beto_alaska@hotmail.com>
2015-05-05 15:53:33 -0500

    Frontend

--- Web Pages/renovarSuscripcion.js
+++ Web Pages/renovarSuscripcion.js
@@ -2,8 +2,22 @@ $(function(){
   $('#check').change(function(){
     if (this.checked) {
       $('#discount').text('Gratis');
+      $('#extra_discount').text('-');
     } else {
       $('#discount').text('Precio Normal');
     };
   });
+
+  $('select').on('change', function(){
+    // var value = parseInt($(this).val());
+    // var num = (value - 1) * 25;
+    // num.toString();
+    var discount = "-";
+    console.log(parseInt($(this).val()));
+    if (parseInt($(this).val()) > 1) {
+     discount = "%25 en años pasados"; 
+    };
+    $('#extra_discount').text(discount);
+  });
+
 });
